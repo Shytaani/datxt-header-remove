fb9263c1ad6b2a695abfee61e5b7727e0243cb1b
Tanishy <mambaout134@gmail.com>
2022-09-11 23:58:38 +0900

    Refactor python source

--- src/python/remove-header.py
+++ src/python/remove-header.py
@@ -1,8 +1,10 @@
 import pandas as pd
 
-with open('test.dat', 'r') as f:
+data_path='../../data'
+
+with open(f'{data_path}/test.dat', 'r') as f:
     df_no_header = pd.DataFrame(pd.read_csv(f, header=1, index_col=0))
     print(df_no_header)
 
-with open('test_no_header.csv', 'w') as f:
+with open(f'{data_path}/test_no_header.csv', 'w') as f:
     df_no_header.to_csv(f)

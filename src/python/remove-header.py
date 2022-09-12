36c997caad8230777d7d758fff5781234af1c173
Tanishy <mambaout134@gmail.com>
2022-09-12 11:28:11 +0900

    Modify python source

--- src/python/remove-header.py
+++ src/python/remove-header.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-data_path='../../data'
+base_path='data'
 
-with open(f'{data_path}/test.dat', 'r') as f:
+with open(f'{base_path}/test.dat', 'r') as f:
     df_no_header = pd.DataFrame(pd.read_csv(f, header=1, index_col=0))
     print(df_no_header)
 
-with open(f'{data_path}/test_no_header.csv', 'w') as f:
+with open(f'{base_path}/test_no_header_python.csv', 'w') as f:
     df_no_header.to_csv(f)
